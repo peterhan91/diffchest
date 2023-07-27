374b26d68b9fa05de94706cfbdd4d094cc50c496
Tianyu Han <petertianyuhan@hotmail.com>
2023-07-27 16:55:15 +0200

    Update run_padchest_cls.py

--- run_padchest_cls.py
+++ run_padchest_cls.py
@@ -2,7 +2,6 @@ from templates_cls import *
 from experiment_classifier import *
 
 if __name__ == '__main__':
-    # need to first train the diffae autoencoding model & infer the latents
     # this requires only a single GPU.
     gpus = [3]
     conf = padchest256_autoenc_cls()
